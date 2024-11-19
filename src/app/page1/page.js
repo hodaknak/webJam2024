32693dc746834577f43bc6df9340cd3d6e459c47
Cadecraft <cadendotlee@gmail.com>
2024-11-19 11:26:59 -0800

    feat: homepage game code button

--- src/app/page1/page.js
+++ src/app/page1/page.js
@@ -2,28 +2,29 @@ export default function Page1() {
     return (
         <div className="w-full text-center">
             <p className="text-xl mt-20">
-                this is page 1
+                Ready for an icebreaker?
             </p>
+            <br/>
+            <div style={{"display": "flex", "justifyContent": "center"}}>
+                <div style={{"width": "40%"}}>
+                    <div className="roundbox">
+                        <input type="text" placeholder="GAME CODE HERE" className="codebox"/>
+                        <br/>
+                        <br/>
+                        <button disabled={true}>Join a game</button>
+                    </div>
+                    <br/>
+                    OR
+                    <br/><br/>
+                    <div className="roundbox">
+                        <button>Create a game</button>
+                    </div>
+                </div>
+            </div>
             <p className="m-40">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-            </p>
-            <p className="m-40">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-            </p>
-            <p className="m-40">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                <b>Credits</b>
+                <br/>This was created for the ICSSC WebJam 2024 at UCI by Caden Lee, Hodaka Nakamura, Kelvin Wu, and Kyle Kim.
+                <br/>Check out the source code on <a className="link-underline" href="https://github.com/hodaknak/webjam2024">GitHub</a>!
             </p>
         </div>
     )
