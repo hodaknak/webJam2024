0705f06cfa9b84d75039846b506ef8a71f6d51a9
Kyle Kim <54655342+Synchro1411@users.noreply.github.com>
2024-11-21 15:36:43 -0800

    sample queries

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ const io = require("socket.io")(server, {
 io.on("connection", (socket) => {
     console.log("Connection established");
 
+
     // allegedly socket.broadcast can be used to send to all other clients
     // socket.id also seems useful
 
@@ -35,17 +36,43 @@ io.on("connection", (socket) => {
         io.emit("msg", msg);
     });*/
 
+    // LIST OF QUERIES
+
+    // These 6 are ran with db.run(). Here is a sample query with the error logging
+    /*db.run(insertGameQuery,data,(err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log("Game Inserted");
+    }*/
+        const insertGameQuery = "INSERT INTO Game(GameCode, Host, Messages, GameState) VALUES(?, ?, ?, ?)"
+        const insertUserQuery = "INSERT INTO Users(id, Username, GameCode, BreakoutRoomCode) VALUES(?, ?, ?, ?)"
+        const insertRoomQuery = "INSERT INTO rooms (RoomID,GameCode) Values(?,?)";
+        const removeGameQuery = "DELETE FROM Game WHERE GameCode = ?";
+        const removeUserQuery = "DELETE FROM Users WHERE id = ?"
+        const removeRoomQuery = "DELETE FROM rooms WHERE RoomID = ? AND GameCode = ?"
+    
+        // These 3 are ran with db.all(). Here is a sample query with the error logging
+        /*db.all(selectRoomQuery,data,(err,rows) => {
+            if (err) {
+              return console.error(err.message);
+            }
+            console.log("Query result: ", rows);
+          })*/
+        const selectGameQuery = "SELECT * FROM Game where GameCode = ?"
+        const selectUserQuery = "SELECT * FROM Users where id = ?"
+        const selectRoomQuery = "SELECT * FROM rooms where RoomID = ? AND GameCode = ?"
+        
+
     socket.on("fetchRoom", (msg) => {
-        console.log(`${socket.id}: ${msg.command} ${msg.name} ${msg}`);
-        if(msg.command === "create room") {
-            const insertRoomQuery = "INSERT INTO rooms (RoomID,GameCode) Values(?,?)";
-            const data = [msg.name,msg.code]
-            console.log("I made it here")
-            db.run(insertRoomQuery,data,(err) => {
+        console.log(`${socket.id}: ${msg.command} ${msg.roomcode} ${msg.gamecode}`);
+        if(msg.command === "find room") {
+            const data = [msg.roomcode,msg.gamecode]
+            db.all(selectRoomQuery,data,(err,rows) => {
                 if (err) {
                   return console.error(err.message);
                 }
-                console.log("Inserted into Rooms Table");
+                console.log("Query result: ", rows);
               })
         }
 
