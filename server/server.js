af1e1505ebb86e3fe98bb3e81960f1e1ea288169
Kyle Kim <54655342+Synchro1411@users.noreply.github.com>
2024-11-20 18:26:42 -0800

    db setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import http from "http";
+import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+
 const server = http.createServer((req, res) => {
    // handle http requests if needed
 });
-
-import { Server } from "socket.io";
 
 const io = new Server(server, {
     cors: {
@@ -19,6 +20,30 @@ io.on("connection", (socket) => {
     // socket.id also seems useful
 
     socket.on("msg", (msg) => {
+        if(msg === "create room") {
+            const data = [
+                "1000",
+                "",
+                "",
+                socket.id,
+                "",
+                "Waiting"
+            ]
+            const insertSql = `INSERT INTO Rooms (GameCode, RoomList, UserList, Host, MessageHistory, GameState) VALUES (?, ?, ?, ?, ?, ?)`;
+            db.run(insertSql,data,function (err) {
+                if (err) {
+                  console.error("Error inserting room into database:", err.message);
+                } else {
+                  console.log(`Room created with GameCode: ${gameCode}`);
+                  // Optionally, emit an event back to the client with the game code
+                  socket.emit("roomCreated", {
+                    gameCode,
+                    message: "Room created successfully!",
+                  });
+                }
+              }
+            );
+        }
         console.log(socket.id + ': ' + msg);
         io.emit("msg", msg);
     });
@@ -31,5 +56,18 @@ io.on("connection", (socket) => {
 server.listen(3001, () => {
     console.log("WebSocket server listening on port 3001");//
 });
+
+
+const db = new sqlite3.Database(
+    "./collection.db", // Specify your database file path
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        return console.error("Database connection error:", err.message);
+      }
+      console.log("Connected to SQLite database.");
+    }
+  );
+
 
 export default io;
